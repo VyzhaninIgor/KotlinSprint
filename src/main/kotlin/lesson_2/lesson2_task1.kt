c546669b3027ef65e22b53cc4f5c43a420168ef0
Igor Vyzhanin <v9828243144@gmail.com>
2024-06-09 17:21:33 +0400

    KS-2-1-find average value of grades in class

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,7 @@
+package lesson_2
+
+fun main() {
+    val studentGrades: IntArray = intArrayOf(3, 4, 3, 5)
+    val averageValue: Double = studentGrades.sum().toDouble() / studentGrades.size
+    println("%.2f".format(averageValue))
+}
