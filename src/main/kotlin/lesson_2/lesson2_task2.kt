823cf0a0dbc6e94b21be183ca5a6502e364d2ba2
Vyzhanin Igor <vyzhaninigor@gmail.com>
2024-06-10 10:59:34 +0400

    KS-2-2-refactor

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -7,12 +7,12 @@ fun main() {
     val traineesSalary: Int = 20_000
 
     val totalSalaryOfStaffersCosts: Int = totalStaffers * staffersSalary
-    val totalSalaryOfAllWorkers: Int = (totalStaffers * staffersSalary) + (totalTrainees * traineesSalary)
-    val averageSalaryInCompany: Int = totalSalaryOfAllWorkers / (totalStaffers + totalTrainees)
+    val totalSalaryOfAllWorkersCosts: Int = (totalSalaryOfStaffersCosts) + (totalTrainees * traineesSalary)
+    val averageSalaryInCompany: Int = totalSalaryOfAllWorkersCosts / (totalStaffers + totalTrainees)
 
     print(
         "Расходы на выплату зарплаты постоянных сотрудников: $totalSalaryOfStaffersCosts\n" +
-                "Общие расходы по ЗП после прихода стажеров: $totalSalaryOfAllWorkers\n" +
+                "Общие расходы по ЗП после прихода стажеров: $totalSalaryOfAllWorkersCosts\n" +
                 "Средняя ЗП одного сотрудника после устройства стажеров: $averageSalaryInCompany"
     )
 }
