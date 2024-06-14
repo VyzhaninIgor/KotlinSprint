56d7d699fa8551168f153992731f3c103c7d6dd3
VyzhaninIgor <vyzhaninigor@gmail.com>
2024-06-14 10:17:37 +0400

    KS-3-5-refactoring

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,11 +3,10 @@ package lesson_3
 fun main() {
     val fullMove: String = "D2-D4;0"
 
-    val startPosition: String = fullMove.split('-')[0]
-    val endPosition: String = fullMove.split('-')[1].split(';')[0]
-    val moveNumber: Int = fullMove.split(';')[1].toInt()
+    val (startPosition: String, endPosition: String, moveNumberStr: String) = fullMove.split('-', ';')
+    val moveNumber: Int = moveNumberStr.toInt()
 
     println(startPosition)
     println(endPosition)
-    println(moveNumber)
+    print(moveNumber)
 }
