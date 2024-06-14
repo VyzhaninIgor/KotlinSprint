1b82b7ad4121d5402e3867b5420dd31a6f78c363
VyzhaninIgor <vyzhaninigor@gmail.com>
2024-06-14 23:23:08 +0400

    KS-4-4-remove useless arguments

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,19 +3,13 @@ package lesson_4
 fun isEven(num: Int): Boolean = num % 2 == 0
 
 fun main() {
-    val dayOfExercises: Int = 2
-
+    val dayOfExercises: Int = 1
     val isTodayEvenDay: Boolean = isEven(dayOfExercises)
 
-    val armsExercises: Boolean = !isTodayEvenDay
-    val legsExercises: Boolean = isTodayEvenDay
-    val backExercises: Boolean = isTodayEvenDay
-    val pressExercises: Boolean = !isTodayEvenDay
-
     print("""
-        Упражнения для рук: $armsExercises
-        Упражнения для ног: $legsExercises
-        Упражнения для спины: $backExercises
-        Упражнения для пресса: $pressExercises
+        Упражнения для рук: ${!isTodayEvenDay}
+        Упражнения для ног: $isTodayEvenDay
+        Упражнения для спины: $isTodayEvenDay
+        Упражнения для пресса: ${!isTodayEvenDay}
         """.trimIndent())
 }
